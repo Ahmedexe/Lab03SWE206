66cf40d1940f77adf4b4ebdcf701f707ce0c1fb4
Ahmedexe <alzuhairahmed@gmail.com>
2024-09-15 22:50:51 +0300

    Reading Student Names

--- App.java
+++ App.java
@@ -1,8 +1,23 @@
+import java.util.Scanner;
+
 public class App {
 
     public static void main(String[] args) {
 
-        System.out.println("ahmed");
+        System.out.println("Please Enter The Number of Students");
+        Scanner input = new Scanner(System.in);
+        
+        int numOfStd = input.nextInt();
+        input.nextLine();  // Consume the leftover newline
+
+        String[] students = new String[numOfStd];
+        System.out.println("Please Enter The Names of the Students");
+
+        for (int i = 0; i < numOfStd; i++) {
+            students[i] = input.nextLine();
+        }
+        input.close();
+
         
     }
 }
